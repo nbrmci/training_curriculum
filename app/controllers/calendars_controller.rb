d705fd21444b2a439aac32c3babfacf1d9696ecc
nbrmci <cr3886165@gmail.com>
2020-07-31 12:27:51 +0900

    Issue1 edit file

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,6 @@ class CalendarsController < ApplicationController
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans }
-      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans}
       @week_days.push(days)
     end
 
